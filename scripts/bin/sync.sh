bdb48766bc122430cb571cffa765425d5c4878ae
SaschaJanssen <sascha.rodekamp@gmail.com>
2022-12-14 08:52:56 +0100

    removeing zip files as they get generated on deployment

--- scripts/bin/sync.sh
+++ scripts/bin/sync.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+echo "Packaging lambda functions first."
+./create_deployment_packages.sh
+
+echo "Sync to $1"
 cd ../..
 aws s3 sync . s3://$1 --exclude "*" --include "scripts/assets/*" --include "functions/*" --include "templates/*" --include "submodules/*" --include "showcase/data/grid_topology/*" --region us-east-1
 cd scripts
