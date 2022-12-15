1ce4e6c6035f841327bee84f2d4c9d1bef7ab207
SaschaJanssen <sascha.rodekamp@gmail.com>
2022-12-15 15:29:16 +0100

    fix cfn lint issues

--- scripts/bin/sync.sh
+++ scripts/bin/sync.sh
@@ -4,5 +4,5 @@ echo "Packaging lambda functions first."
 
 echo "Sync to $1"
 cd ../..
-aws s3 sync . s3://$1 --exclude "*" --include "scripts/assets/*" --include "functions/*" --include "templates/*" --include "submodules/*" --include "showcase/data/grid_topology/*" --region us-east-1
+aws s3 sync . s3://$1 --exclude "*" --include "scripts/assets/*" --include "scripts/layers/*" --include "functions/*" --include "templates/*" --include "submodules/*" --include "showcase/data/grid_topology/*" --region us-east-1
 cd scripts
