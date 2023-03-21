ac0d4ba8412dbd1103680eb5bf1095144a01f69a
SaschaJanssen <sascha.rodekamp@gmail.com>
2023-03-21 16:57:04 +0100

    update lambda package script

--- scripts/bin/create_deployment_packages.sh
+++ scripts/bin/create_deployment_packages.sh
@@ -12,6 +12,8 @@ do
    function_name=${lambda_folder////_}
    function_name=${function_name//source_/}
    echo $function_name
+
+   mkdir -p $HOME/packages/${function_name}
    (cd source/$lambda_folder; zip -9qr "$HOME/packages/${function_name}/lambda.zip" .;cd $HOME)
 done
 
